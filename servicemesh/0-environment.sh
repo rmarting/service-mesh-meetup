dd52d96a5ff5b8f7fe33e5498984ef99f218064c
cvicens <carlos.vicens.alonso@gmail.com>
2019-02-04 12:50:14 +0100

    changes to deploy msa specific branch GIT_REF env var added

--- servicemesh/0-environment.sh
+++ servicemesh/0-environment.sh
@@ -4,13 +4,13 @@
 export MINISHIFT_PROFILE="coolstore"
 export MINISHIFT_MEMORY="10GB"
 export MINISHIFT_CPUS="2"
-export MINISHIFT_VM_DRIVER="kvm" # xhyve | virtualbox | kvm
+export MINISHIFT_VM_DRIVER="xhyve" # xhyve | virtualbox | kvm
 export MINISHIFT_DISK_SIZE="40g"
 
 # Istio
 export ISTIO_VERSION=1.0.5
 export ISTIO_PROJECT_NAME="istio-system"
-export OS="linux" # osx | linux
+export OS="osx" # osx | linux
 
 # Kiali
 export KIALI_VERSION="v0.10.0"
@@ -23,3 +23,4 @@ export GRAFANA_URL="https://grafana-${ISTIO_PROJECT_NAME}.$(minishift ip).nip.io
 export INGRESS_URL="http://istio-ingressgateway-${ISTIO_PROJECT_NAME}.$(minishift ip).nip.io"
 #export SECURE_INGRESS_URL="https://istio-ingressgateway-${ISTIO_PROJECT_NAME}.$(minishift ip).nip.io"
 export MSA_PROJECT_NAME=coolstore
+export GIT_REF="istio-1.0.5"
