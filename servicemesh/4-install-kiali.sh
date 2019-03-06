3a8ab1fc36b099bd75bcbf25c1365a52bf8cc8d0
cvicens <carlos.vicens.alonso@gmail.com>
2019-03-06 13:50:51 +0100

    adding command to get urls from grafana and jaeger with oc

--- servicemesh/4-install-kiali.sh
+++ servicemesh/4-install-kiali.sh
@@ -5,6 +5,9 @@
 
 echo "Deploy Kiali Definitions ..."
 
+export JAEGER_URL="$(oc get route jaeger-query -n ${ISTIO_SYSTEM_NAMESPACE} | awk 'NR>1 {printf ($5 == "edge") ? "https://%s" : "http://%s",$2 }')"
+export GRAFANA_URL="$(oc get route grafana -n ${ISTIO_SYSTEM_NAMESPACE} | awk 'NR>1 {printf ($5 == "edge") ? "https://%s" : "http://%s",$2 }')" 
+
 for yaml in secret configmap serviceaccount clusterrole clusterrolebinding deployment service route ingress crds
 do
   curl https://raw.githubusercontent.com/kiali/kiali/${KIALI_VERSION}/deploy/openshift/${yaml}.yaml | envsubst |  oc apply -n ${ISTIO_SYSTEM_NAMESPACE} -f -
