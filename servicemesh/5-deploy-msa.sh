cccd9018d83ae7b2add16bc19a818bb3ed1f3be8
cvicens <carlos.vicens.alonso@gmail.com>
2019-03-06 13:51:39 +0100

    adding command to get url for COOLSTORE_GW_ENDPOINT and jaeger with oc

--- servicemesh/5-deploy-msa.sh
+++ servicemesh/5-deploy-msa.sh
@@ -7,5 +7,9 @@ oc new-project ${MSA_PROJECT_NAME}
 
 oc adm policy add-scc-to-user privileged -z default -n ${MSA_PROJECT_NAME}
 
-oc process -f ../microservices/coolstore-msa-template.yaml | \
+GIT_URI="https://github.com/rmarting/service-mesh-meetup"
+GIT_REF="master"
+COOLSTORE_GW_ENDPOINT="http://$(oc get route istio-ingressgateway -n ${ISTIO_SYSTEM_NAMESPACE} | awk 'NR>1 {print $2}')"
+
+oc process -p GIT_URI=${GIT_URI} -p GIT_REF=${GIT_REF} -p COOLSTORE_GW_ENDPOINT=${COOLSTORE_GW_ENDPOINT} -f ../microservices/coolstore-msa-template.yaml | \
   oc apply -n ${MSA_PROJECT_NAME} -f -
